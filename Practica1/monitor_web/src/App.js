bc8ee606ad596bb91515f5b0553b116261fd5129
airton47 <3002614110101@ingenieria.usac.edu.gt>
2021-02-15 19:59:48 -0600

    pruebas web

--- Practica1/monitor_web/src/App.js
+++ Practica1/monitor_web/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Info from './componentes/Info';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Info />
+        <p>        
           MONITOR DE SMART WRISTBAND!
         </p>
         <a
--- Practica1/monitor_web/src/componentes/Info.js
+++ Practica1/monitor_web/src/componentes/Info.js
@@ -11,12 +11,45 @@ class Info extends Component{
         };
     }
 
-    render(){
-        return (
-            <div>
+    componentDidMount() {
+        fetch("http://ec2-3-16-13-115.us-east-2.compute.amazonaws.com/tests/")
+          .then(res => res.json())
+          .then(
+            (json) => {
+              this.setState({
+                isLoaded: true,
+                items: json
+              });
+            },
+            // Nota: es importante manejar errores aquí y no en 
+            // un bloque catch() para que no interceptemos errores
+            // de errores reales en los componentes.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }
 
-            </div>
-        );
+    render(){
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+        return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+        return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                {items.map(item => (
+                    <li key={item._id}>
+                    ID: {item._id} |VVALOR: {item.valor} | CREACION: {item.createdAt}
+                    </li>
+                ))}
+                </ul>
+            );
+        }        
     }
 }
 
