27b2c314c6193322e29d2a7679837a79ea2bbd2c
airton47 <3002614110101@ingenieria.usac.edu.gt>
2021-02-15 19:36:14 -0600

    web info test

---
+++ Practica1/monitor_web/src/componentes/Info.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Info extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Info;
