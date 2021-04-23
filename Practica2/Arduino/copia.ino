b10040a9f929d53266ebd6b529a7bbd5eed0c9f6
airton47 <3002614110101@ingenieria.usac.edu.gt>
2021-04-23 09:28:07 -0600

    cambios para arduino, codigo funcional con app y web, terminado

--- Practica2/Arduino/copia.ino
+++ Practica2/Arduino/copia.ino
@@ -100,3 +100,86 @@ void pulseCounter()
   // Increment the pulse counter
   pulseCount++;
 }
+
+/*------------------------------------------------------------------------------*/
+22/04/2021 15:09
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(10,11);
+int PinSensor = 2;              //Sensor conectado en el pin 2
+
+volatile int NumPulsos;         //variable para la cantidad de pulsos recibidos
+float factor_conversion=7.11;   //para convertir de frecuencia a caudal
+float volumen=0;                //Variable que guarda el volumen que se registra en un periodo de tiempo
+float prevVolumen = -1;           //Variable temporal que guarda el volumen previo medido
+float prevVolumen2 = -1;        //Variable temporal para guardar el anterior del anterior(prevVolumen)
+long dt=0;                      //variación de tiempo por cada bucle
+long t0=0;                      //millis() del bucle anterior
+int numToMul = 1;               //Numero para determinar si es positivo o negativo el volumen
+const float tiempoCause = 0.1;          //en segundos 's')Tiempo transcurrido en la medicion de cause para calculo de volumen
+const int periodo = 0;             //Periodo de tiempo para hacer retardo con millis()
+unsigned long tiempoAhora = 0;  //Tiempo actual, para calculo de periodo de tiempo en retardo con millis()
+
+void setup() 
+{  
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  pinMode(PinSensor, INPUT); 
+  attachInterrupt(0,ContarPulsos,RISING);//(Interrupción 0(Pin2),función,Flanco de subida)
+  //t0=millis();
+} 
+
+void loop ()    
+{
+  //if (Serial.available()) {
+  //  if(Serial.read()=='r')volumen=0;//restablecemos el volumen si recibimos 'r'
+  //}
+  float frecuencia=ObtenerFrecuecia(); //obtenemos la frecuencia de los pulsos en Hz
+  float caudal_L_m=frecuencia/factor_conversion; //calculamos el caudal en L/m
+  //dt=millis()-t0; //calculamos la variación de tiempo
+  //t0=millis();
+  //volumen=(caudal_L_m/60)*(dt/1000)*1000; // volumen(mL)=caudal(L/s)*tiempo(s)*1000
+  volumen=(caudal_L_m/60)*(tiempoCause)*1000; // volumen(mL)=caudal(L/s)*tiempo(s)*1000
+
+  //condicion para cambiar el signo del volumen
+  if(volumen == 0){//solo para cuando el volumen ha sido cero
+    if(prevVolumen !=0){//pero como este hacer cero en mas de una ocacion se verifica que el anterior no lo sea
+      if(prevVolumen2 != 0){//ya que a veces hay error se verifica que el anter. del anter. no sea cero tampoco
+        numToMul = numToMul*(-1);
+      }
+    }
+  }
+  
+  //Serial.print ("Caudal: ");
+  //Serial.print (caudal_L_m,3);
+  //Serial.print ("L/min\tVolumen: "); 
+  /*ENVIO DE DATOS AL BLUETOOTH*/  
+  //if(millis() > tiempoAhora + periodo){
+  //  tiempoAhora = millis();
+    
+    //Serial.println();
+    //Serial.println(" mL");
+  //}
+  
+  prevVolumen2 = prevVolumen;
+  prevVolumen = volumen;
+  Serial.print((int)volumen*numToMul);
+  delay(100);
+}
+
+//Función que se ejecuta en interrupción
+void ContarPulsos ()  
+{ 
+  NumPulsos++;  //incrementamos la variable de pulsos
+} 
+
+//Función para obtener frecuencia de los pulsos
+int ObtenerFrecuecia() 
+{
+  int frecuencia;
+  NumPulsos = 0;      //Ponemos a 0 el número de pulsos
+  interrupts();       //Habilitamos las interrupciones
+  delay(100);         //muestra de 1 segundo
+  noInterrupts();     //Deshabilitamos  las interrupciones
+  frecuencia=NumPulsos; //Hz(pulsos por segundo)
+  return frecuencia/0.1;
+}
--- Practica2/Arduino/py1_sensorflujo/py1_sensorflujo.ino
+++ Practica2/Arduino/py1_sensorflujo/py1_sensorflujo.ino
@@ -1,80 +1,71 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTSerial(10,11);
-int PinSensor = 2;              //Sensor conectado en el pin 2
 
-volatile int NumPulsos;         //variable para la cantidad de pulsos recibidos
-float factor_conversion=7.11;   //para convertir de frecuencia a caudal
-float volumen=0;                //Variable que guarda el volumen que se registra en un periodo de tiempo
+/*
+YF‐ S201 Water Flow Sensor
+Water Flow Sensor output processed to read in litres/hour
+Adaptation Courtesy: hobbytronics.co.uk
+*/
+volatile int flow_frequency; // Measures flow sensor pulses
+
+
+float l_minute = 0;               //variable con cuenta de frecuencia medida desde el sensor
+float vol = 0;                    //variable del volumen calculado
 float prevVolumen = -1;           //Variable temporal que guarda el volumen previo medido
-float prevVolumen2 = -1;        //Variable temporal para guardar el anterior del anterior(prevVolumen)
-long dt=0;                      //variación de tiempo por cada bucle
-long t0=0;                      //millis() del bucle anterior
-int numToMul = 1;               //Numero para determinar si es positivo o negativo el volumen
-const float tiempoCause = 0.1;          //en segundos 's')Tiempo transcurrido en la medicion de cause para calculo de volumen
-const int periodo = 0;             //Periodo de tiempo para hacer retardo con millis()
-unsigned long tiempoAhora = 0;  //Tiempo actual, para calculo de periodo de tiempo en retardo con millis()
+float prevVolumen2 = -1;          //Variable temporal para guardar el anterior del anterior(prevVolumen)
+//>>>>HAY 3 VARIABLES DE VOLUMEN PARA HACER COMPROBACIONES Y EVITAR ERRORES AL ENVIAR LOS DATOS
+//>>>>ESTO PORQUE EL SENSOR NO ES MUY PRECISO
 
-void setup() 
-{  
-  Serial.begin(9600);
-  BTSerial.begin(9600);
-  pinMode(PinSensor, INPUT); 
-  attachInterrupt(0,ContarPulsos,RISING);//(Interrupción 0(Pin2),función,Flanco de subida)
-  //t0=millis();
-} 
+unsigned char flowsensor = 2;     // Sensor Input
+unsigned long currentTime;        //tiempo transcurrido desde que inicio arduino
+unsigned long cloopTime;          //tiempo en el ciclo
+const float tiempoCause = 0.2;    //(segundos 's')Tiempo transcurrido en la medicion de cause para calculo de volumen
+int numToMul = -1;                //Numero para determinar si es positivo o negativo el volumen
+const int periodo = 200;          //numero de milisegundos del periodo
 
-void loop ()    
+
+void setup()
 {
-  //if (Serial.available()) {
-  //  if(Serial.read()=='r')volumen=0;//restablecemos el volumen si recibimos 'r'
-  //}
-  float frecuencia=ObtenerFrecuecia(); //obtenemos la frecuencia de los pulsos en Hz
-  float caudal_L_m=frecuencia/factor_conversion; //calculamos el caudal en L/m
-  //dt=millis()-t0; //calculamos la variación de tiempo
-  //t0=millis();
-  //volumen=(caudal_L_m/60)*(dt/1000)*1000; // volumen(mL)=caudal(L/s)*tiempo(s)*1000
-  volumen=(caudal_L_m/60)*(tiempoCause)*1000; // volumen(mL)=caudal(L/s)*tiempo(s)*1000
-
-  //condicion para cambiar el signo del volumen
-  if(volumen == 0){//solo para cuando el volumen ha sido cero
-    if(prevVolumen !=0){//pero como este hacer cero en mas de una ocacion se verifica que el anterior no lo sea
-      if(prevVolumen2 != 0){//ya que a veces hay error se verifica que el anter. del anter. no sea cero tampoco
-        numToMul = numToMul*(-1);
-      }
-    }
-  }
-  
-  //Serial.print ("Caudal: ");
-  //Serial.print (caudal_L_m,3);
-  //Serial.print ("L/min\tVolumen: "); 
-  /*ENVIO DE DATOS AL BLUETOOTH*/  
-  //if(millis() > tiempoAhora + periodo){
-  //  tiempoAhora = millis();
-    
-    //Serial.println();
-    //Serial.println(" mL");
-  //}
-  
-  prevVolumen2 = prevVolumen;
-  prevVolumen = volumen;
-  Serial.print((int)volumen*numToMul);
-  delay(100);
+   pinMode(flowsensor, INPUT);
+   Serial.begin(9600);
+   BTSerial.begin(9600);
+   attachInterrupt(digitalPinToInterrupt(flowsensor), flow, RISING); // Setup Interrupt
+   currentTime = millis();
+   cloopTime = currentTime;
 }
 
-//Función que se ejecuta en interrupción
-void ContarPulsos ()  
-{ 
-  NumPulsos++;  //incrementamos la variable de pulsos
-} 
 
-//Función para obtener frecuencia de los pulsos
-int ObtenerFrecuecia() 
+void loop ()
 {
-  int frecuencia;
-  NumPulsos = 0;      //Ponemos a 0 el número de pulsos
-  interrupts();       //Habilitamos las interrupciones
-  delay(100);         //muestra de 1 segundo
-  noInterrupts();     //Deshabilitamos  las interrupciones
-  frecuencia=NumPulsos; //Hz(pulsos por segundo)
-  return frecuencia/0.1;
+   //interrupts();
+   //delay(periodo);
+   //noInterrupts();   
+   currentTime = millis();
+   // Every second, calculate and print litres/hour
+   if(currentTime >= (cloopTime + periodo)){
+    cloopTime = currentTime; // Updates cloopTime
+    
+    // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+    l_minute = (flow_frequency / 7.5); //
+    vol = (l_minute/60)*(tiempoCause)*1000;
+    if(vol == 0){//solo para cuando el volumen ha sido cero
+      if(prevVolumen != 0){//pero como este hacer cero en mas de una ocacion se verifica que el anterior no lo sea
+        if(prevVolumen2 != 0){//ya que a veces hay error se verifica que el anter. del anter. no sea cero tampoco
+          numToMul = numToMul*(-1);//Se hace el toggle del signo del volumen
+        }
+      }
+    }
+    
+    int VOL = (int)(vol*numToMul);
+    Serial.println(VOL);
+    
+    prevVolumen2 = prevVolumen;   //Se transfiere el volumen previo de prevVolumen
+    prevVolumen = vol;            //Se tranfiere volumen medido a volumen_previo
+    flow_frequency = 0;           // Reset Counter
+   }
+}
+
+void flow () // Interrupt function
+{
+   flow_frequency++;
 }
